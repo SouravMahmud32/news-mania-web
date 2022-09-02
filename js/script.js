da96c8af929936d24efa3b720d00848e9623bd7b
sourav-mahmud <souravmahmud32@gmail.com>
2022-09-03 04:59:06 +0600

    menu added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('hello')
+const loadAllNews = async() =>{
+    const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await response.json();
+    return data.data.news_category;
+}
+
+const setAllMenu = async() =>{
+    const data = await loadAllNews();
+    const menu = document.getElementById('all-menu');
+    data.forEach((news) =>{
+        console.log(news);
+        const li = document.createElement('li');
+            li.innerHTML = `<a>${news.category_name}</a>`;
+            menu.appendChild(li);
+    });
+}
+setAllMenu();
