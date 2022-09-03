09deb3bdf00b8cfebca39913009b187a4fd5da0a
sourav-mahmud <souravmahmud32@gmail.com>
2022-09-04 01:32:59 +0600

    error handler added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 const loadAllNews = async () => {
-  const response = await fetch(
-    `https://openapi.programming-hero.com/api/news/categories`
-  );
-  const data = await response.json();
-  return data.data.news_category;
+  try{
+    const response = await fetch(
+      `https://openapi.programming-hero.com/api/news/categories`
+    );
+    const data = await response.json();
+    return data.data.news_category;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const setAllMenu = async () => {
@@ -102,3 +106,4 @@ const showModal = (details, thumbnail_url, name)=>{
     </p>
     `;
 }
+
